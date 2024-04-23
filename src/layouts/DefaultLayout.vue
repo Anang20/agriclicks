f52f44578fbcd97a319ffcb149db254056fa4f7d
anangsyah <anang.haqim@ist.id>
2024-04-23 15:04:02 +0700

    chore: setup default layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppMenu from "@/components/AppMenu.vue";
+import AppHeader from "@/components/AppHeader.vue";
+
+export default defineComponent({
+    name: "DefaultLayout",
+    components: {
+        AppMenu,
+        AppHeader,
+    },
+});
+</script>
+
+<template>
+  <v-app dark class="drawer">
+    <AppMenu/>
+    <AppHeader/>
+    <v-main class="m-6">
+        <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
