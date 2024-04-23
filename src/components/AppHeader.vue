1bf9d2b0d5d537dd621566169c7469825d8c9026
anangsyah <anang.haqim@ist.id>
2024-04-23 15:03:06 +0700

    chore: add app header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app-bar :elevation="5">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon
+          @click="$store.dispatch('toggleDrawer')"
+        ></v-app-bar-nav-icon>
+      </template>
+      <template v-slot:append>
+        <v-avatar color="primary">
+          <v-icon icon="mdi-account-circle"></v-icon>
+        </v-avatar>
+      </template>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  </script>
+  
