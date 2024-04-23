69e6406c265c6ded2b5502d4ed15a06bf32a9fb0
anangsyah <anang.haqim@ist.id>
2024-04-23 15:32:37 +0700

    chore: add utils rupiah formatted

---
+++ src/utils/rupiahFormatted.js
@@ -0,0 +1,10 @@
+export default function rupiahFormatted(value) {
+    const formatter = new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+  
+    return formatter.format(value);
+}
