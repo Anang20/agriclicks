b74a4546b8e128e6fde7c0026df1b5462c8d5309
anangsyah <anang.haqim@ist.id>
2024-04-23 15:23:06 +0700

    chore: add modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-dialog v-bind:v-model="isOpen" max-width="500">
+      <v-card rounded="lg">
+        <v-card-title class="d-flex justify-space-between align-center">
+          <div class="text-h5 text-medium-emphasis ps-2">{{ title }}</div>
+          <v-btn @click="handleClose" icon="mdi-close" variant="text"></v-btn>
+        </v-card-title>
+        <v-divider
+          class="mb-4 border-2 border-solid border-b-gray-600"
+        ></v-divider>
+  
+        <slot></slot>
+      </v-card>
+    </v-dialog>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+    title: String,
+  });
+  
+  const emit = defineEmits(['close']);
+  
+  const handleClose = () => {
+    emit('close');
+    isOpen = false;
+  };
+  </script>
+  
